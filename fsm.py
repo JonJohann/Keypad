1812d55c441bd075e8e155f2e17d832b032cce5c
TheMangelo <mikkelangeloj@gmail.com>
2019-10-15 13:40:17 +0200

    Update fsm.py

--- fsm.py
+++ fsm.py
@@ -1,4 +1,6 @@
+
 """This is the finite state machine"""
+import kpc
 
 class Rule:
     """Ruleclass"""
@@ -61,16 +63,28 @@ class FSM:
 
         self.KPC = kpc
         self.rules = [
-            Rule('S_init', 'S-read1', self.all_symbols, self.KPC.reset_password_accumulator),
+            Rule('S_init', 'S-read1', self.all_symbols, self.KPC.init_passcode_entry),
             Rule('S-read1', 'S-read1', self.all_digits, self.KPC.append_next_password_digit),
             Rule('S-read1', 'S-verify', '*', self.KPC.verify_login),
-            Rule('S-read1', 'S-init', self.all_digits, self.KPC.reset_agent),
-            Rule('S-verify', 'S-init', self.all_digits, self.KPC.reset_agent),
+            Rule('S-read1', 'S-init', '#', self.KPC.init_passcode_entry),
+            Rule('S-verify', 'S-init', 'N', self.KPC.reset_agent),
             Rule('S-verify', 'S-Active', 'Y', self.KPC.fully_activate_agent),
 
+            Rule('S-Active', 'S-Light', self.all_digits, self.KPC.set_light),
+            Rule('S-Light', 'S-Active', self.all_digits, self.KPC.light_led),
+            Rule('S-Light','S-Light', '#*',self.KPC.flash_leds),
+
+            Rule('S-Active', 'S-init', '#', self.KPC.exit_action),
             Rule('S-Active', 'S-read2', '*', self.KPC.reset_password_accumulator),
             Rule('S-read2', 'S-Active', '#', self.KPC.refresh),
             Rule('S-read2', 'S-read2', self.all_digits, self.KPC.append_next_password_digit),
             Rule('S-read2', 'S-read3', '*', self.KPC.cache_first_new_passord),
             Rule('S-read3', 'S-read3', self.all_digits, self.KPC.append_next_password_digit),
             Rule('S-read3', 'S-Active', '*', self.KPC.compare_new_password)]
+
+if __name__ == '__main__':
+    b = kpc.KPCAgent()
+    a = FSM(b)
+
+    a.main_loop()
+
